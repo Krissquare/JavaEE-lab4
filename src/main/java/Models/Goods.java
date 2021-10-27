925a3562ad2dd7c389acc8f2f996d04c8deaa0d5
Krissquare <734195698@qq.com>
2021-10-27 17:42:30 +0800

    5

---
+++ src/main/java/Models/Goods.java
@@ -0,0 +1,14 @@
+package Models;
+
+public class Goods {
+
+    private Integer id;
+    private String name;
+    private String goodsSn;
+    private String imageUrl;
+    private Integer state;
+    private String gmtCreate;
+    private String gmtModified;
+    private Boolean disable;
+
+}
---
+++ src/main/java/Models/Product.java
@@ -0,0 +1,24 @@
+package Models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private Integer id;
+    private String name;
+    private String skuSn;
+    private String detail;
+    private String imageUrl;
+    private Integer originalPrice;
+    private Integer price;
+    private Integer inventory;
+    private Integer state;
+    private Double weight;
+    private String gmtCreate;
+    private String gmtModified;
+    private Goods goods;
+    private Boolean disable;
+    private Boolean shareable;
+
+}
